76231f94ac15aa1abfd3401f6c5cf771e37488fb
M. Findley <123705549+MFindley08@users.noreply.github.com>
2023-09-23 23:25:21 -0400

    installed streamlit

--- main.py
+++ main.py
@@ -39,4 +39,15 @@ Answer: The next term in the sequence is 162.
 Question: {question}
 """
 
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+def generate_response(question):
+    chat = ChatOpenAI(temperature=0.0, openai_api_key=openai_api_key)
+    prompt_template = ChatPromptTemplate.from_template(template=prompt)
+    messages = prompt_template.format_messages(
+        question=question
+    )
+    response = chat(messages)
+    return response.content
 
