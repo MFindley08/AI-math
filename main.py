a789548596db8d88f34b8217f1bd1c98dfc2595f
M. Findley <123705549+MFindley08@users.noreply.github.com>
2023-10-05 12:05:47 -0400

    updated answer and clues display

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def index():
-  return render_template("index.html")
-
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
-
 from langchain.llms import OpenAI
 
-llm = OpenAI(openai_api_key="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")
-
 st.set_page_config(page_title="Sum = Thin', Your Friendly Math Mentor")
-
-openai_api_key = st.sidebar.text_input('OpenAI API Key', key="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", type='password')
-
+st.title ("Your Friendly Math Mentor, Sum=Thin'")
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
 prompt = """
 Serve as a math and arithmetic reasoning guide, 
@@ -31,35 +20,38 @@ Here are some examples with answers and clues:
 
 Question:  John has 5 apples, and Sarah gives him 3 more. 
 How many apples does John have now?
-
-Clues: To find the total number of apples John has now, add 
-the apples he had initially (5) to the apples Sarah gave 
+Answer: John has 8 apples.
+Clues: 1.To find the total number of apples John has now, add 
+the apples he had initially. 2. (5) to the apples Sarah gave 
 him (3).
 
-Answer: John has 8 apples.
+
 
 Question:  If the length of a rectangle is 8 cm and the width 
 is 5 cm, what is its perimeter?
-Clues: To find the perimeter of a rectangle, add the lengths 
-of all four sides. For a rectangle, opposite sides are equal in length.
 Answer: The perimeter of the rectangle is 26 cm.
+Clues: 1.To find the perimeter of a rectangle, add the lengths 
+of all four sides. \
+2. For a rectangle, opposite sides are equal in length.
+
 
 Question: Solve for x: (1/2)x + 4 = 8.
-Clues: To isolate x, first, subtract 4 from both sides of the equation. Then, multiply both sides by 2 (the reciprocal of 1/2).
 Answer: x = 8.
+Clues: 1.To isolate x, first, subtract 4 from both sides of the equation. \
+2.Then, multiply both sides by 2 (the reciprocal of 1/2). 
+
 
 Question: What is the next number in the sequence? 
-Given Sequence: 2, 6, 18, 54, ...
-Clues: Common Ratio: 3
-Algorithm Application:
-Input r = 3 and the last term, which is 54.
-Calculate Next_term = 54 * 3 = 162.
 Answer: The next term in the sequence is 162.
+Given Sequence: 2, 6, 18, 54, ...
+Clues: 1. Common Ratio: 3 \
+2. Algorithm Application:
+Input r = 3 and the last term, which is 54. \
+3. Calculate Next_term = 54 * 3 = 162.
+
 
 Question: {question}
 """
-
-
 def generate_response(question):
     chat = ChatOpenAI(temperature=0.0, openai_api_key=openai_api_key)
     prompt_template = ChatPromptTemplate.from_template(template=prompt)
