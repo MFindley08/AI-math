794d720754174ca804ec98e12d563e2caa8e0ad5
M. Findley <123705549+MFindley08@users.noreply.github.com>
2024-05-25 16:50:46 -0400

    Create main copy.py

---
+++ main copy.py
@@ -0,0 +1,72 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+
+st.set_page_config(page_title="Sum = Thin', Your Friendly Math Mentor")
+st.title("AI Math Mentor")
+openai_api_key = st.sidebar.text_input('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', type='password')
+
+
+prompt = """
+Serve as a math and arithmetic reasoning guide, 
+assisting students in resolving mathematical
+inquiries. Students will present their questions, 
+and your task is to methodically work through each 
+problem, recording every logical step along the way. 
+You may be prompted to reveal the solution or provide 
+hints to guide students toward their own solution.
+
+Here are some examples with answers and clues:
+
+Question:  John has 5 apples, and Sarah gives him 3 more. 
+How many apples does John have now?
+
+Clue: To find the total number of apples John has now, add 
+the apples he had initially (5) to the apples Sarah gave 
+him (3).
+
+Answer: John has 8 apples.
+
+Question:  If the length of a rectangle is 8 cm and the width 
+is 5 cm, what is its perimeter?
+Clue: To find the perimeter of a rectangle, add the lengths 
+of all four sides. For a rectangle, opposite sides are equal in length.
+Answer: The perimeter of the rectangle is 26 cm.
+
+Question: Solve for x: (1/2)x + 4 = 8.
+Clue: To isolate x, first, subtract 4 from both sides of the equation. Then, multiply both sides by 2 (the reciprocal of 1/2).
+Answer: x = 8.
+
+Question: What is the next number in the sequence? 
+Given Sequence: 2, 6, 18, 54, ...
+Clue: Common Ratio: 3
+Algorithm Application:
+Input r = 3 and the last term, which is 54.
+Calculate Next_term = 54 * 3 = 162.
+Answer: The next term in the sequence is 162.
+
+Question: {question}
+"""
+
+
+def generate_response(question):
+    chat = ChatOpenAI(temperature=0.0, openai_api_key=	
+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)
+    prompt_template = ChatPromptTemplate.from_template(template=prompt)
+    messages = prompt_template.format_messages(
+        question=question
+    )
+    response = chat(messages)
+    return response.content
+
+with st.form('myform'):
+    question = st.text_input('Enter question:', '')
+    clues = st.form_submit_button('Give me clues')
+    answer = st.form_submit_button('Show me the answer')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if clues and openai_api_key.startswith('sk-'):
+        st.info(generate_response(question).split("Clues")[1][2:])
+    if answer and openai_api_key.startswith('sk-'):
+        st.info(generate_response(question).split("Clues")[0])
+
